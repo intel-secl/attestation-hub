8816c2a3cab4e27a100c528891fc76bc45978da4
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-10-15 01:05:01 -0700

    verify certificate chain for flavor signing

--- features/attestation-hub-setup/src/main/java/com/intel/mtwilson/attestationhub/setup/CreateUserKeystore.java
+++ features/attestation-hub-setup/src/main/java/com/intel/mtwilson/attestationhub/setup/CreateUserKeystore.java
@@ -7,19 +7,26 @@ package com.intel.mtwilson.attestationhub.setup;
 import com.intel.dcsg.cpg.tls.policy.TlsConnection;
 import com.intel.dcsg.cpg.tls.policy.TlsPolicy;
 import com.intel.dcsg.cpg.tls.policy.TlsPolicyBuilder;
-import com.intel.dcsg.cpg.tls.policy.impl.InsecureTlsPolicy;
+import com.intel.dcsg.cpg.x509.X509Util;
 import com.intel.kms.setup.JettyTlsKeystore;
 import com.intel.mtwilson.Folders;
 import com.intel.mtwilson.attestationhub.common.AttestationHubConfigUtil;
 import com.intel.mtwilson.attestationhub.common.Constants;
 import com.intel.mtwilson.client.jaxrs.CaCertificates;
 import com.intel.mtwilson.core.common.utils.AASTokenFetcher;
+import com.intel.mtwilson.privacyca.v2.model.CaCertificateFilterCriteria;
+import com.intel.mtwilson.shiro.ShiroUtil;
+import com.intel.mtwilson.util.ResourceFinder;
+import org.apache.commons.io.IOUtils;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.URL;
+import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -32,6 +39,7 @@ public class CreateUserKeystore extends JettyTlsKeystore {
     private String url;
     private String aasApiUrl;
     private final String trustStorePath = Folders.configuration()+"/truststore.";
+    private final String caCertPath = Folders.configuration() + File.separator + "cms-ca.cert";
 
     @Override
     protected void configure() throws Exception {
@@ -83,11 +91,30 @@ public class CreateUserKeystore extends JettyTlsKeystore {
         try {
             tlsConnection = new TlsConnection(new URL(url), tlsPolicy);
             CaCertificates certClient = new CaCertificates(clientConfiguration, tlsConnection);
-            X509Certificate samlCertificate = certClient.retrieveCaCertificate("saml");
+            CaCertificateFilterCriteria criteria = new CaCertificateFilterCriteria();
+            criteria.domain = "saml";
+            String cert = certClient.searchCaCertificatesPem(criteria);
+            List<X509Certificate> samlCertificateChain = X509Util.decodePemCertificates(cert);
+
+            X509Certificate samlCertificate = samlCertificateChain.get(0);
+            verifySamlCertChain(samlCertificateChain, samlCertificate);
             storeCertificate(samlCertificate, String.format("%s(%s)", samlCertificate.getSubjectX500Principal().getName(), "saml"), trustStoreFileName);
-        } catch (Exception ex) {
+        } catch(GeneralSecurityException e) {
+            log.error("Error verifying signature: ", e);
+        } catch(Exception ex) {
             log.error("Error during retrieval of certificates for writing to the key store.", ex);
         }
+    }
+
+    private void verifySamlCertChain(List<X509Certificate> samlCertificateChain, X509Certificate samlCertificate) throws IOException, GeneralSecurityException {
+        ArrayList<Certificate> intermediateCas = new ArrayList<>();
+        intermediateCas.add(samlCertificateChain.get(1));
+
+        ArrayList<Certificate> rootCas = new ArrayList<>();
+        InputStream rootCert = new FileInputStream(ResourceFinder.getFile(caCertPath));
+        rootCas.addAll(X509Util.decodePemCertificates(IOUtils.toString(rootCert)));
+
+        ShiroUtil.verifyCertificateChain(samlCertificate, rootCas, intermediateCas);
     }
 
     private void storeCertificate (X509Certificate certificate, String alias, String trustStoreFileName) throws Exception {
